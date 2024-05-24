0645d23fd09c5d02d2c82abc1157fec759b49c0a
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2024-05-25 00:39:22 +0300

    Repository Design First Step
    Repository Design First Step

---
+++ OnlineMuhasebeServer/OnlineMuhasebeServer.Domain/Repositories/ICommandRepository.cs
@@ -0,0 +1,16 @@
+﻿using OnlineMuhasebeServer.Domain.Abstraction;
+
+namespace OnlineMuhasebeServer.Domain.Repositories
+{
+    public interface ICommandRepository<T> : IRepository<T>
+        where T : Entity
+    {
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+        Task RemoveById(string id);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ OnlineMuhasebeServer/OnlineMuhasebeServer.Domain/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineMuhasebeServer.Domain.Abstraction;
+
+namespace OnlineMuhasebeServer.Domain.Repositories
+{
+    public interface IRepository<T>
+        where T : Entity
+    {
+        void CreateDbContextInstance(DbContext context);
+        DbSet<T> Entity { get; set; }
+    }
+}
