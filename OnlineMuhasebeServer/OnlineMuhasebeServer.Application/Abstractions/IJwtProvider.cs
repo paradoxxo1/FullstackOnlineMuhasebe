ac9cadc22306359a77034cead6761c2883f4457d
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2024-05-28 19:36:56 +0300

    Json Web Token, Login, Options Pattern
    Json Web Token, Login, Options Pattern Part-1

---
+++ OnlineMuhasebeServer/OnlineMuhasebeServer.Application/Abstractions/IJwtProvider.cs
@@ -0,0 +1,9 @@
+﻿using OnlineMuhasebeServer.Domain.AppEntities.Identity;
+
+namespace OnlineMuhasebeServer.Application.Abstractions
+{
+    public interface IJwtProvider
+    {
+        string CreateToken(AppUser user, List<string> roles);
+    }
+}
---
+++ OnlineMuhasebeServer/OnlineMuhasebeServer.Infrasturcture/Authentication/JwtProvider.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using OnlineMuhasebeServer.Application.Abstractions;
+using OnlineMuhasebeServer.Domain.AppEntities.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace OnlineMuhasebeServer.Infrasturcture.Authentication
+{
+    public sealed class JwtProvider : IJwtProvider
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public JwtProvider(IOptions<JwtOptions> jwtOptions)
+        {
+            _jwtOptions = jwtOptions.Value;
+        }
+
+        public string CreateToken(AppUser user, List<string> roles)
+        {
+            var claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.NameLastName),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(ClaimTypes.Authentication, user.Id),
+                new Claim(ClaimTypes.Role, String.Join(",", roles))
+            };
+
+            JwtSecurityToken jwtSecurityToken = new(
+                issuer: _jwtOptions.Issuer,
+                audience: _jwtOptions.Audience,
+                claims: claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey)), SecurityAlgorithms.HmacSha256));
+
+            string token = new JwtSecurityTokenHandler()
+                .WriteToken(jwtSecurityToken);
+
+            return token;
+        }
+    }
+}
