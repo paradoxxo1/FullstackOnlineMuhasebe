065e258a38a7667e4792ec0cedc27f92d0531c4e
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2024-05-29 22:46:40 +0300

    Roller için sql idenetity table yapısı düzenleme
    Roller için sql idenetity table yapısı düzenleme

---
+++ OnlineMuhasebeServer/OnlineMuhasebeServer.Application/Features/AppFeatures/RoleFeatures/Commands/CreateRole/CreateRoleHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using OnlineMuhasebeServer.Domain.AppEntities.Identity;
+
+namespace OnlineMuhasebeServer.Application.Features.AppFeatures.RoleFeatures.Commands.CreateRole
+{
+    public sealed class CreateRoleHandler : IRequestHandler<CreateRoleRequest, CreateRoleResponse>
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public CreateRoleHandler(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<CreateRoleResponse> Handle(CreateRoleRequest request, CancellationToken cancellationToken)
+        {
+            AppRole role = await _roleManager.Roles.Where(p => p.Code == request.Code).FirstOrDefaultAsync();
+            if (role != null) throw new Exception("Bu rol daha önce kayıt edilmiş! ");
+
+            role = new AppRole
+            {
+                Id = Guid.NewGuid().ToString(),
+                Code = request.Code,
+                Name = request.Name,
+            };
+
+            await _roleManager.CreateAsync(role);
+            return new();
+        }
+    }
+}
---
+++ OnlineMuhasebeServer/OnlineMuhasebeServer.Prenstation/Controller/RolesController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OnlineMuhasebeServer.Application.Features.AppFeatures.RoleFeatures.Commands.CreateRole;
+using OnlineMuhasebeServer.Prenstation.Abstraction;
+
+namespace OnlineMuhasebeServer.Prenstation.Controller
+{
+    public class RolesController : ApiController
+    {
+        public RolesController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> CreateRole(CreateRoleRequest request)
+        {
+            CreateRoleResponse response = await _mediator.Send(request);
+            return Ok(response);
+
+        }
+    }
+}
