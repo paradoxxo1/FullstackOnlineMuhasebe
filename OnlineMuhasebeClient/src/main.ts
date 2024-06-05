8da823a173f85ed5558f0c23b8e6f9dcc5b4260d
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2024-06-05 23:05:50 +0300

    Guard, Validation, Http ve Error Service added
    Guard, Validation, Http ve Error Service added

--- OnlineMuhasebeClient/src/main.ts
+++ OnlineMuhasebeClient/src/main.ts
@@ -2,15 +2,20 @@ import { BrowserModule, bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { importProvidersFrom } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { AuthGuard } from "./app/ui/components/auth/guards/auth.guard";
+import {provideHttpClient} from "@angular/common/http";
+
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideHttpClient(),
     importProvidersFrom(
       BrowserModule,
       RouterModule.forRoot([
         {
           path: "",
           loadComponent: () => import("./app/ui/components/layouts/layouts.component").then(c => c.LayoutsComponent),
+          canActivateChild: [AuthGuard],
           children : [
             {
               path: "",
