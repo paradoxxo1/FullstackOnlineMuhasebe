a6b4bbd63437147deb339638c01c16ddf40101f0
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2024-06-09 01:07:18 +0300

    Refresh Token and Refactoring
    Refresh Token and Refactoring

--- OnlineMuhasebeServer/OnlineMuhasebeServer.Application/Features/AppFeatures/AuthFeatures/Commands/Login/LoginCommandHandler.cs
+++ OnlineMuhasebeServer/OnlineMuhasebeServer.Application/Features/AppFeatures/AuthFeatures/Commands/Login/LoginCommandHandler.cs
@@ -6,45 +6,44 @@ using OnlineMuhasebeServer.Domain.AppEntities;
 using OnlineMuhasebeServer.Domain.AppEntities.Identity;
 using OnlineMuhasebeServer.Domain.Dtos;
 
-namespace OnlineMuhasebeServer.Application.Features.AppFeatures.AuthFeatures.Commands.Login
+namespace OnlineMuhasebeServer.Application.Features.AppFeatures.AuthFeatures.Commands.Login;
+
+public class LoginQueryHandler : ICommandHandler<LoginCommand, LoginQueryResponse>
 {
-    public class LoginQueryHandler : ICommandHandler<LoginCommand, LoginQueryResponse>
+    private readonly IJwtProvider _jwtProvider;
+    private readonly UserManager<AppUser> _userManager;
+    private readonly IAuthService _authService;
+
+    public LoginQueryHandler(IJwtProvider jwtProvider, UserManager<AppUser> userManager, IAuthService authService)
     {
-        private readonly IJwtProvider _jwtProvider;
-        private readonly UserManager<AppUser> _userManager;
-        private readonly IAuthService _authService;
+        _jwtProvider = jwtProvider;
+        _userManager = userManager;
+        _authService = authService;
+    }
 
-        public LoginQueryHandler(IJwtProvider jwtProvider, UserManager<AppUser> userManager, IAuthService authService)
-        {
-            _jwtProvider = jwtProvider;
-            _userManager = userManager;
-            _authService = authService;
-        }
+    public async Task<LoginQueryResponse> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        AppUser user = await _authService.GetByEmailOrUserNameAsync(request.EmailOrUserName);
+        if (user == null) throw new Exception("Kullanıcı bulunamadı! ");
 
-        public async Task<LoginQueryResponse> Handle(LoginCommand request, CancellationToken cancellationToken)
-        {
-            AppUser user = await _authService.GetByEmailOrUserNameAsync(request.EmailOrUserName);
-            if (user == null) throw new Exception("Kullanıcı bulunamadı! ");
+        var checkUser = await _authService.CheckPasswordAsync(user, request.Password);
+        if (!checkUser) throw new Exception("Şifreniz Yanlış! ");
 
-            var checkUser = await _authService.CheckPasswordAsync(user, request.Password);
-            if (!checkUser) throw new Exception("Şifreniz Yanlış! ");
+        IList<UserAndCompanyRelationship> companies = await _authService.GetCompanyListByUserIdAsync(user.Id);
+        if (companies.Count() == 0) throw new Exception("Herhangi bir şirkete kayıtlı değilsiniz!");
 
-            IList<UserAndCompanyRelationship> companies = await _authService.GetCompanyListByUserIdAsync(user.Id);
-            if (companies.Count() == 0) throw new Exception("Herhangi bir şirkete kayıtlı değilsiniz!");
+        IList<CompanyDto> companiesDto = companies.Select(s => new CompanyDto(
+            s.Company.Id, s.Company.Name)).ToList();
 
-            IList<CompanyDto> companiesDto = companies.Select(s => new CompanyDto(
-                s.Company.Id, s.Company.Name)).ToList();
+        LoginQueryResponse response = new(
+            Token: await _jwtProvider.CreateTokenAsync(user),
+            Email: user.Email,
+            UserId: user.Id,
+            NameLastName: user.NameLastName,
+            Companies: companiesDto,
+            Company: companiesDto[0],
+            Year: DateTime.Now.Year);
 
-            LoginQueryResponse response = new(
-                Token: await _jwtProvider.CreateTokenAsync(user),
-                Email: user.Email,
-                UserId: user.Id,
-                NameLastName: user.NameLastName,
-                Companies: companiesDto,
-                Company: companiesDto[0],
-                Year: DateTime.Now.Year);
-
-            return response;
-        }
+        return response;
     }
 }
--- OnlineMuhasebeServer/OnlineMuhasebeServer.Presentation/Controller/AuthController.cs
+++ OnlineMuhasebeServer/OnlineMuhasebeServer.Presentation/Controller/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using OnlineMuhasebeServer.Application.Features.AppFeatures.AuthFeatures.Commands.GetTokenByRefreshToken;
 using OnlineMuhasebeServer.Application.Features.AppFeatures.AuthFeatures.Commands.Login;
 using OnlineMuhasebeServer.Application.Features.AppFeatures.AuthFeatures.Queries.GetRolesByUserIdAndCompanyId;
 using OnlineMuhasebeServer.Prenstation.Abstraction;
@@ -16,6 +17,13 @@ namespace OnlineMuhasebeServer.Prenstation.Controller
         public async Task<IActionResult> Login(LoginCommand request)
         {
             LoginQueryResponse response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> GetTokenByRefreshToken(GetTokenByRefreshTokenCommand request)
+        {
+            GetTokenByRefreshTokenCommandResponse response = await _mediator.Send(request);
             return Ok(response);
         }
 
